18ad97bed9aac27937a9b6a8b1d59eca0d888110
aveliz1999 <github@veliz99.com>
2021-02-15 16:27:18 -0500

    Add error messages for overloaded resources

--- src/components/StatsBars/StatsBars.tsx
+++ src/components/StatsBars/StatsBars.tsx
@@ -28,36 +28,64 @@ export default function StatsBars({currentCPU, maxCPU, currentMemory, maxMemory,
 
     return <div className={styles.info}>
         <div className={styles.infoEntry}>
-            <div>
-                CPU Cores:
-                {currentCPU}
-                /
-                {maxCPU}
-            </div>
+            {
+                currentCPU >= 0 ? <>
+                    <div>
+                        CPU Cores:
+                        {currentCPU}
+                        /
+                        {maxCPU}
+                    </div>
 
-            <div className={[styles.bar, styles.cpuBar].join(' ')} style={{width: `${cpu}%`}}/>
+                    <div className={[styles.bar, styles.cpuBar].join(' ')} style={{width: `${cpu}%`}}/>
+                </> : <div>
+                    <div>
+                        CPU Error: Usage of {maxCPU - currentCPU} cores exceeds maximum of {maxCPU} cores
+                    </div>
+                    <div className={[styles.bar, styles.cpuBar].join(' ')} style={{width: '100%', background: 'red'}}/>
+                </div>
+
+            }
         </div>
 
         <div className={styles.infoEntry}>
-            <div>
-                RAM (GB):
-                {currentMemory}
-                /
-                {maxMemory}
-            </div>
+            {
+                currentMemory >= 0 ? <>
+                    <div>
+                        RAM (GB):
+                        {currentMemory}
+                        /
+                        {maxMemory}
+                    </div>
 
-            <div className={[styles.bar, styles.memoryBar].join(' ')} style={{width: `${memory}%`}}/>
+                    <div className={[styles.bar, styles.memoryBar].join(' ')} style={{width: `${memory}%`}}/>
+                </> : <div>
+                    <div>
+                        Memory Error: Usage of {maxMemory - currentMemory}GB exceeds maximum of {maxMemory}GB
+                    </div>
+                    <div className={[styles.bar, styles.cpuBar].join(' ')} style={{width: '100%', background: 'red'}}/>
+                </div>
+            }
         </div>
 
         <div className={styles.infoEntry}>
-            <div>
-                Storage (GB):
-                {currentStorage}
-                /
-                {maxStorage}
-            </div>
+            {
+                currentStorage >= 0 ? <>
+                    <div>
+                        Storage (GB):
+                        {currentStorage}
+                        /
+                        {maxStorage}
+                    </div>
 
-            <div className={[styles.bar, styles.storageBar].join(' ')} style={{width: `${storage}%`}}/>
+                    <div className={[styles.bar, styles.storageBar].join(' ')} style={{width: `${storage}%`}}/>
+                </> : <div>
+                    <div>
+                        Storage Error: Usage of {maxStorage - currentStorage}GB exceeds maximum of {maxStorage}GB
+                    </div>
+                    <div className={[styles.bar, styles.cpuBar].join(' ')} style={{width: '100%', background: 'red'}}/>
+                </div>
+            }
         </div>
     </div>
 }
